e71c226cc3a728fa1df3ee1e9f58a8124eac905e
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-18 16:37:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,7 +48,9 @@ class Song
   end 
   
   def self.new_from_filename(name)
-    array = name.split(" - ")
-    array[1] = array[1].chomp(".mp3")
+    song = self.new 
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    song 
   end 
 end
